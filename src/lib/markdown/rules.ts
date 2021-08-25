63176331288d9fb05cb5e611a1e885f654561a76
JiangWeixian <jiangweixian1994@gmail.com>
2021-08-25 16:26:24 +0800

    perf: use multimd-table npm

--- src/lib/markdown/rules.ts
+++ src/lib/markdown/rules.ts
@@ -6,8 +6,7 @@ import breakPlugin from "./breaks";
 import tablesPlugin from "./tables";
 import noticesPlugin from "./notices";
 import underlinesPlugin from "./underlines";
-
-import multimdTablePlugin from "./multimdTable";
+import multimdTablePlugin from "markdown-it-multimd-table";
 
 export default function rules({ embeds, rules = {} }) {
   return markdownit("default", {
