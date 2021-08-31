31a286c79c317d7f3a6cb865bd3b6af1e927966b
JiangWeixian <jiangweixian1994@gmail.com>
2021-08-31 13:41:47 +0800

    fix: state selection undefine

--- src/queries/getCellCount.ts
+++ src/queries/getCellCount.ts
@@ -3,7 +3,9 @@ import { selectedRect } from 'prosemirror-tables'
 
 export default function getCellCount (state: EditorState) {
   return {
-    total: selectedRect(state).map.cellsInRect(selectedRect(state)).length,
+    total: state.selection
+      ? selectedRect(state).map.cellsInRect(selectedRect(state)).length
+      : 0,
     // TODO: count td/th count
     // th: thCount,
     // td: tdCount,
